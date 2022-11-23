31fc63be641d633773f0e97e68c0758794e045d9
Andre Ricardo <andrepraz@gmail.com>
2022-11-23 19:52:43 -0300

    23112022

--- estoque/notaEntrada/views.py
+++ estoque/notaEntrada/views.py
@@ -28,8 +28,29 @@ def notaEntradaNew(request):
         }
         return render(request, template_name, context)
 
-def notaEntradaUpdate(request):
-    pass
+def notaEntradaUpdate(request, pk):
+    entrada = NotasEntradas.objects.get(id=pk)
+    quantidade_nota_antiga = entrada.quantidade
+    if request.method == 'POST':
+        form = NotasEntradasForm(request.POST, instance=entrada)
+        if form.is_valid():
+            form.save(commit=False)
+            form.cleaned_data['produto'].quantidade = form.cleaned_data['produto'].quantidade - quantidade_nota_antiga + form.cleaned_data['quantidade']
+            form.cleaned_data['produto'].save_base()
+            form.save()
+            return redirect('entrada:notaEntradaList')
+    else:
+        template_name = 'notaEntradaUpdate.html'
+        context = {
+            'form': NotasEntradasForm(instance=entrada),
+            'pk': pk
+        }
+        return render(request, template_name, context)
 
-def notaEntradaDelete(request):
-    pass
+def notaEntradaDelete(request, pk):
+    entrada = NotasEntradas.objects.get(id=pk)
+    quantidade_nota_antiga = entrada.quantidade
+    entrada.produto.quantidade = entrada.produto.quantidade - quantidade_nota_antiga
+    entrada.produto.save()
+    entrada.delete()
+    return redirect('entrada:notaEntradaList')
